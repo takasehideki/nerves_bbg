5511d4dc5e72821cd73af40ac3363b690c58a2d6
takasehideki <takase@i.kyoto-u.ac.jp>
2020-05-06 17:52:25 +0900

    mix nerves.new

---
+++ lib/nerves_bbg.ex
@@ -0,0 +1,18 @@
+defmodule NervesBbg do
+  @moduledoc """
+  Documentation for NervesBbg.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesBbg.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
